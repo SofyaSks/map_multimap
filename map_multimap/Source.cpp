e210237b533836b01ae699ee50dd3dbf5c625d1b
Nazar <nazar.r.mak@gmail.com>
2022-12-27 22:17:22 +0300

    Добавьте файлы проекта.

---
+++ map_multimap/Source.cpp
@@ -0,0 +1,149 @@
+﻿////// класс MAP /////////////
+//template <class Key, class T, class Comp = less<key>, class Allocator = Allocator<pair<const key, T>>>
+//    class map
+//    /****************************/
+//    explicit map(const Comp &cmpfn = Comp(),Allocator &a = Allocator());
+//
+// map(map <Key, T, Comp, Allocator> &ob);
+//
+//template < class InIter> 
+//map(InIter start,InIter end, const Comp &cmpfn = Comp(),const Allocator &a = Allocator()); 
+//
+///*******************************/
+//    ■■ ==
+//    ■■ <
+//    ■■ <=
+//    ■■ !=
+//    ■■ >
+//    ■■ >=
+
+        /*****************************/
+
+    // 1 iterator begin();
+    // const_iterator begin() const;
+
+    // 2 void clear();
+
+    // 3 size_type count(const key_type &k) const;
+
+
+
+    // 5 const_iterator end() const;
+    //   iterator end();
+
+    // 6 pair<iterator, iterator> equal_range(const key_type &k);
+    //   pair<const_iterator, const_iterator> equal_range(const key_type &k) const;
+
+    // 7 void erase(iterator start, iterator end);
+
+    // 8 pair<iterator, bool> insert(const value_type &val);
+
+    // 9 reference operator[](const key_type &i);
+
+    // 10 size_type size() const;
+
+    // 11 void swap(map<Key, T, Comp, Allocator> &ob);
+
+        /******************************/
+        //Пример: данный пример демонстрирует методы
+        //работы со отображениями
+//#include <iostream>
+//#include <map>
+//#include <vector>
+//using namespace std;
+//void main()
+//{
+//    map <int, string> our_map;
+//    /*vector <int> our_vector;
+//    cout << "\n\nmax size of vector --> "
+//        << our_vector.max_size() / sizeof(int);
+//    cout << "\n\nmax size of map --> "
+//        << our_map.max_size() / sizeof(int);
+//    cout << "\n\n-------------------------------\n";*/
+//    string val;
+//    int key;
+//
+//    for (int i = 0; i < 5; i++)
+//    {
+//     cout << "\nInput value : ";
+//     cin >> val;
+//     cout << "\nInput key : ";
+//     cin >> key;
+//     pair<int, string> element(key, val);
+//     our_map.insert(element);
+//    }
+//    
+//
+//    /*cout << "\nCurrent element of map --> "
+//        << our_map.size() << endl;*/
+//    /*cout << "\n\n-------------------------------\n";
+//    cout << "\nInput value : ";
+//    cin >> val;
+//    cout << "\nInput key : ";
+//    cin >> key;*/
+//
+//
+//    /*pair<map<int, string>::iterator, bool>
+//        err = our_map.insert(make_pair(key, val));
+//    if (err.second == false)
+//    {
+//        cout << "\nError !!!\n";
+//    }
+//    cout << "\nCurrent element of map --> "
+//        << our_map.size() << endl;*/
+//
+//    map<int, string>::iterator iter = our_map.begin();
+//    for (; iter != our_map.end(); iter++)
+//    {
+//        cout << "\nKey --> " << iter->first
+//            << "\t\tValue --> " << iter->second;
+//    }
+//    cout << "\n---------------------------------\n";
+//
+//    // Var 1
+//    //iter = our_map.begin();
+//    //iter = our_map.find(3);
+//    //our_map.erase(iter);
+//
+//    // Var 2
+//    iter = our_map.begin();
+//    for (; iter != our_map.end(); iter++)
+//    {
+//        if (iter->first == 3) {
+//            iter = our_map.erase(iter);
+//        }
+//
+//    }
+//
+//    iter = our_map.begin();
+//    for (; iter != our_map.end(); iter++)
+//    {
+//        cout << "\nKey --> " << iter->first
+//            << "\t\tValue --> " << iter->second;
+//    }
+//    cout << "\n---------------------------------\n"; 
+
+    
+   /* cout << "Inter key to delete value: ";
+    int el;
+    cin >> el;
+    for (; iter != our_map.end(); iter++)
+    {
+        if (iter->first == el) {
+            our_map.erase(iter);
+            break;
+        }
+       
+    }
+
+    cout << "\n---------------------------------\n";
+    
+
+    for (; iter != our_map.end(); iter++)
+    {
+        cout << "\nKey --> " << iter->first
+            << "\t\tValue --> " << iter->second;
+    }
+    cout << "\n---------------------------------\n";
+
+}*/
---
+++ map_multimap/multimap.cpp
@@ -0,0 +1,128 @@
+﻿//////// класс multimap///////////////////
+
+//template <class Key, class T, class Comp = less<key>,
+//	class Allocator = Alloctor<pair<const key, T> >
+//	class multimap
+
+//	/**********************/
+//	explicit multimap(const Comp &cmpfn =Comp(), Allocator &a = Allocator());
+//
+//multimap(multimap<Key, T, Comp, Allocator> &ob);
+//
+//template < class InIter> multimap(InIter start,InIter end, const Comp &cmpfn = Comp(),
+//	const Allocator &a = Allocator());
+
+/************************************/
+ //   ■■ ==
+ // 	■■ <=
+	//■■ !=
+	//■■ >
+	//■■ >=
+
+/***************************/
+// 1 iterator begin();
+//    const_iterator begin() const;
+
+// 2 size_type count(const key_type &k) const;
+
+// 3 bool empty() const;
+
+// 4 const_iterator end() const;
+//   iterator end();
+
+// 5 pair<iterator, iterator> equal_range(const key_type &k);
+//   pair<const_iterator, const_iterator> equal_range(const key_type &k) const
+
+// 6 void erase(iterator start, iterator end);
+
+// 7 size_type erase(const key_type &k);
+
+// 8 pair<iterator, bool> insert(const value_type &val);
+
+// 9 iterator lower_bound(const key_type &k);
+//   const_iterator lower_bound(const key_type &k) const;
+
+// 10 void swap(multimap<Key, T, Comp, Allocator> &ob);
+
+// 11 iterator upper_bound(const key_type &k);
+//    const_iterator upper_bound(const key_type &k) const;
+/**************************************/
+#include <iostream>
+#include <map> 
+#include <string>
+using namespace std;
+template<class container> void show(container col)
+{
+	for (auto i = col.begin(); i != col.end(); ++i)
+	{
+		cout << i->first << '\t' << i->second << endl;
+	}
+	cout << endl << endl;
+}
+void main()
+{
+	//cout << "map\n\n";
+	//map<string, int> cont;
+	//multimap<string, int> multicont;
+	//cont.insert(pair<string, int>("Ivanov", 10));
+	//cont.insert(pair<string, int>("Petrov", 20));
+	//cont["Sidorov"] = 30; // если ключ не существует то добавляется, а если существует то значение переписывается
+	//show(cont);
+	//cont["Ivanov"] = 50;
+	//show(cont);
+	//cont.insert(pair<string, int>("Ivanov", 100));
+	//show(cont);
+
+	///////////////////////////////////////////////////
+
+	//cout << "------------------------\nmultimap\n\n";
+	//multicont.insert(pair<string, int>("Ivanov", 10));
+	//multicont.insert(pair<string, int>("Petrov", 20));
+	//multicont.insert(pair<string, int>("Sidorov", 20));
+
+	////Для мульти отображения не определен оператор "[]"
+	////multicont["Sidorov"] = 30; //Error
+	//
+	//show(multicont);
+	//multicont.insert(pair<string, int>("Ivanov", 100));
+	//show(multicont);
+	//multimap<string, int>::iterator iter =multicont.find("Petrov");
+	//cout << iter->first << '\t' << iter->second<< endl << endl;
+	//cout << "Count of key \"Ivanov\" in multimap = "<< multicont.count("Ivanov") << endl;
+	//iter = multicont.lower_bound("Ivanov");
+	//for (; iter != multicont.upper_bound("Ivanov")&& iter != multicont.end(); iter++)
+	//{
+	//	cout << iter->first << '\t' << iter->second	<< endl;
+	//}
+	//cout << endl << endl;
+
+
+	cout << "map\n\n";
+	map<string, int> street;
+	int n, people, key;
+	string house;
+	cout << "Entere houses number: " << endl;
+	cin >> n;
+	cout << "Enter house number and people: " << endl;
+	for (int i = 1; i <= n; i++)
+	{
+		cout << i << " house ";
+		cin >> house;
+		cin >> people;
+		street.insert(make_pair(house, people));
+	}
+
+	cout << endl << endl;
+
+	show(street);
+
+	cout << endl << endl;
+
+	map<string, int>::iterator iter = street.begin(); 
+
+	cout << "Enter house number: ";
+	
+	if (street.count(house) {
+
+	})
+}
